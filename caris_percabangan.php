4ec8296f8ef9c312407997af318cebafdf818574
nailacrs <nailacarissa9@gmail.com>
2025-02-25 09:03:44 +0700

    Add files via upload

---
+++ caris_percabangan.php
@@ -0,0 +1,18 @@
+<?php
+    $nilai = 120;
+    
+    if ($nilai > 100) {
+        echo "$nilai = Yah! Nilainya gak valid nih:(";
+    }   else if ($nilai >= 90) {
+        echo "$nilai = A";
+    }   else if ($nilai >= 80) {
+        echo "$nilai = B";
+    }   else if ($nilai >= 70) {
+        echo "$nilai = C";
+    }   else if ($nilai >= 0) {
+        echo "$nilai = D";
+    }
+
+    
+?>
+
